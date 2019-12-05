17527d143908da5c532a730f4981418bb63a93ae
Jakob Guddas <jakob.guddas@openmailbox.org>
2019-12-05 11:21:40 +0100

    added detectRetina option

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import 'leaflet/dist/leaflet.css'
 
 import './style.css'
 
-tileLayer('https://raw.githubusercontent.com/jguddas/drawn-hannover/master/tiles/{z}/{x}/{y}.png', { maxZoom: 16, minZoom: 10 })
+tileLayer('https://raw.githubusercontent.com/jguddas/drawn-hannover/master/tiles/{z}/{x}/{y}.png', { maxZoom: 16, minZoom: 10, detectRetina: true })
   .addTo(map(document.body, { zoomControl: false, attributionControl: false })
     .setView([52.3798, 9.7614], 14))
 
